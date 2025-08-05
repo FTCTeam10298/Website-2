7ce21392e8c39971c01628422807357d6c287e2e
Fairhain <109926071+Fairhain@users.noreply.github.com>
2025-08-05 14:29:32 -0700

    added picture on contact us

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -7,6 +7,15 @@ function Contact() {
             <p>
                 For inquiries, please email us at <a href="mailto:ftc10298@gmail.com ">ftc10298@gmail.com </a>
             </p>
+            <img style={
+                {
+                    marginTop: "20px",
+                    borderRadius: "10px",
+                    maxWidth: "50%",
+                    height: "auto",
+                }
+            }src="src/components/contact/tem.jpeg" alt="Contact Us Illustration" className="contact-image"/>
+            
         </div>
     )
 }
