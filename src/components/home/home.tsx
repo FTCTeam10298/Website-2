8fa2bec583d00896cc3d4aedda6e43038d135ea5
Fairhain <109926071+Fairhain@users.noreply.github.com>
2026-09-18 20:40:48 -0700

    brutalist

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,13 +2,22 @@ import './home.css';
 
 function Home() {
     return (
-            
-            <div className="welcome">
+        <div className="welcome">
+            <div className="welcome-inner">
                 <div className="text">
-                    <h2>HI! WE'RE FTC TEAM 10298 BRAINSTORMZ.</h2>
+                    <span className="tag">FTC Team 10298</span>
+                    <h2>HI! WE'RE <span>BRAIN STORMZ.</span></h2>
                     <p>Brain Stormz is a private, non-profit robotics team competing in FIRST Tech Challenge (FTC).</p>
+                    <div className="hero-btns">
+                        <a className="btn btn-primary" href="/team">Meet the Team</a>
+                        <a className="btn btn-ghost" href="/about">Learn More</a>
+                    </div>
+                </div>
+                <div className="photo-frame">
+                    <img src="/home_assets/brainstormzteam.png" alt="Brain Stormz team"/>
                 </div>
             </div>
+        </div>
     );
 }
 
--- src/components/sponsors/sponsors.tsx
+++ src/components/sponsors/sponsors.tsx
@@ -1,25 +1,39 @@
 import './sponsors.css'
 
+const sponsors = [
+    { name: "FathomWerx", url: "https://www.fathomwerx.com/", img: "fathomwerx.png" },
+    { name: "FirstMile", url: "https://www.firstmile.com/", img: "firstmile.png" },
+    { name: "Gene Haas Foundation", url: "https://www.haascnc.com/content/ghf/en/home.html", img: "genehaas.png" },
+    { name: "Holler", img: "hollerLogo.png", dark: true },
+    { name: "Marvell", url: "https://www.marvell.com/", img: "marvell.png" },
+    { name: "Porter & Associates", url: "http://portercivil.com/HOME.html", img: "PA.png" },
+    { name: "Thousand Oaks Presbyterian Church", url: "https://www.topresbyterian.com/", img: "TOPC.png" },
+    { name: "Matter Labs", url: "https://www.matterlabs.co/", img: "matterLabs.png" },
+    { name: "Horizon", img: "horizon.png" },
+    { name: "Sisson Properties", img: "sisson.png" },
+];
+
 function Sponsors() {
     return (
         <div className="content">
             <h2 className='title toppage'>Thank You To All of Our Sponsors!</h2>
             <div className='sponsor-img'>
-            <a href='https://www.fathomwerx.com/'><img src="/sponsor_assets/fathomwerx.png"/></a>
-            <a href='https://www.firstmile.com/'><img src="/sponsor_assets/firstmile.png"/></a>
-            <a href='https://www.haascnc.com/content/ghf/en/home.html'><img src="/sponsor_assets/genehaas.png"/></a>
-            <a className="bg"><img src="/sponsor_assets/hollerLogo.png"/></a>
-            <a href='https://www.marvell.com/'><img src="/sponsor_assets/marvell.png"/></a>
-            <a href='http://portercivil.com/HOME.html'><img src="/sponsor_assets/PA.png"/></a>
-            <a href='https://www.topresbyterian.com/'><img src="/sponsor_assets/TOPC.png"/></a>
-            <a href='https://www.matterlabs.co/'><img src="/sponsor_assets/matterLabs.png"/></a>
-            <a><img src="/sponsor_assets/horizon.png"/></a>
-            <a><img src="/sponsor_assets/sisson.png"/></a>
-
+                {sponsors.map((sponsor) => (
+                    <a
+                        key={sponsor.name}
+                        className={sponsor.dark ? 'sponsor-tile bg' : 'sponsor-tile'}
+                        href={sponsor.url}
+                        target={sponsor.url ? '_blank' : undefined}
+                        rel={sponsor.url ? 'noopener noreferrer' : undefined}
+                    >
+                        <img src={`/sponsor_assets/${sponsor.img}`} alt={sponsor.name} />
+                        <span className='sponsor-name'>{sponsor.name}</span>
+                    </a>
+                ))}
             </div>
 
         </div>
     )
-}   
+}
 
 export default Sponsors;
